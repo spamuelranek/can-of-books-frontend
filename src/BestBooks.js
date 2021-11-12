2812d33881c3977c646f62a8a8889ed2fc3f08d0
Samuel Panek <samuelrpanek@gmail.com>
2021-11-12 13:25:25 -0800

    should address the problems with lab 12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,17 +2,16 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Cards from './Cards.js';
 import UpdateModal from './UpdateModal.js';
-import axios from 'axios';
 
 class BestBooks extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {books: [], modalId: {},  show: false };
+    this.state = {modalId: {},  show: false };
   }
 
   componentDidMount() {
-    this.getBooks();
+    this.props.getBooks();
   }
 
   openModal = (modalCard) => {
@@ -25,27 +24,15 @@ class BestBooks extends React.Component {
     this.setState({ show: false });
   }
 
-  
-  getBooks = async () => {
-    let url = `${process.env.REACT_APP_SERVER_URL}/books`;
-    try {
-      const response = await axios.get(url);
-      console.log('inside try of getBooks');
-      this.setState({books: response.data});
-    } catch (e) {
-      console.error(e.response);
-      }
-  }
-
 
   render() {
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-        {this.state.books.length > 0 ? (
+        {this.props.books.filter(book => book.email === this.props.user.email).length > 0  ? (
           <Carousel variant='dark'>
             {
-              this.state.books.filter(book => book.email === this.props.user.email).map(book => {
+              this.props.books.filter(book => book.email === this.props.user.email).map(book => {
                 return (
                   <Carousel.Item key={book._id}>
                     <Cards openModal = {this.openModal} user={this.props.user} book={book} deleteBook={this.props.deleteBook} />
@@ -57,7 +44,7 @@ class BestBooks extends React.Component {
         ) : (
           <h3>Book collection is empty(</h3>
         )}
-        <UpdateModal updateBook = {this.props.updateBook} getBooks = {this.getBooks} user = {this.props.user} modalId = {this.state.modalId} closeModal = {this.closeModal} show = {this.state.show}/> 
+        <UpdateModal updateBook = {this.props.updateBook} getBooks = {this.props.getBooks} user = {this.props.user} modalId = {this.state.modalId} closeModal = {this.closeModal} show = {this.state.show}/> 
       </>
     )
   }
